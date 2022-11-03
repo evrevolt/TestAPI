0720482add2c6aa4db811fd5b16f655f154e3485
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-03 19:15:37 +0300

    Create JSON Decoder

--- TestAPI/ViewControllers/ViewController.swift
+++ TestAPI/ViewControllers/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getApiButtonTapped() {
+        
+        guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any?safe-mode") else { return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let programmer = try JSONDecoder().decode(Programmer.self, from: data)
+                print(programmer)
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+        
+        
+    }
+    
 }
 
